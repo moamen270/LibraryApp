f580e345a6e29ab5c8872456238a6f678b896ce8
moamen270 <mmoamen270@gmail.com>
2021-10-01 22:19:06 +0200

    Add new Class Employee

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp
+{
+    abstract class Employee : Person
+    {
+        private static int SerialNumber = 95671020;
+        public int NumberOfBorrowBooks = 0;
+        public double Salary { get; }
+
+        public static int NumberOfMembers
+        {
+            get
+            {
+                return SerialNumber - 95671020;
+            }
+        }
+        public Employee(string Name, Gender gender, DateTime Birthday, int Phone, string Address,double Salary)
+            : base(SerialNumber, Name, Phone, Address, Birthday, gender)
+        {
+            this.Salary = Salary;
+            SerialNumber++;
+        }
+    }
+}
