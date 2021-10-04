e94b41d8dbe584e005628e3ca9044f3128884c09
Moamen Mostafa <mmoamen270@gmail.com>
2021-10-04 12:45:13 +0200

    Add nwe class to fix author Bug

---
+++ PersonWithContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp
+{
+    abstract class PersonWithContact : Person
+    {
+        public string Phone { get; }
+        public string Email { get; }
+
+        public PersonWithContact(string _Name, int SerialNumber, string _Phone, string _Email, Gender _Gender, DateTime _Birthday)
+            : base(_Name, SerialNumber, _Gender, _Birthday)
+        {
+            this.Phone = _Phone;
+            this.Email = _Email;
+        }
+    }
+}
